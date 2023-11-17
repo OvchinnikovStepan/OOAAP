a5bce294e1c3fdb2c620b77b02fa978f9dcd3ed8
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-11-17 22:47:58 +0600

    Измение базы для работы(устранена ошибка)

--- spacebattle/SpaceBattle.Lib/ICommand.cs
+++ spacebattle/SpaceBattle.Lib/ICommand.cs
@@ -1,27 +1,6 @@
-using Moq;
+﻿namespace SpaceBattle.Lib;
 
-namespace SpaceBattle.Lib.Tests;
-
-public class MoveCommandTest
+public interface ICommand
 {
-
-    [Fact]
-    public void MoveCommandPositive()
-    {
-        // pre
-        var movable = new Mock<IMovable>();
-
-        movable.SetupGet(m => m.Position).Returns(new int[] { 12, 5 }).Verifiable();
-        movable.SetupGet(m => m.Velocity).Returns(new int[] { -7, 3 }).Verifiable();
-
-        ICommand moveCommand = new MoveCommand(movable.Object);
-
-        // action
-        moveCommand.Execute();
-
-        //post
-        // movable.Position is correct
-        movable.VerifySet(m => m.Position = new int[] { 5, 8 }, Times.Once);
-        movable.VerifyAll(); // !!!
-    }
+    public void Execute();
 }
