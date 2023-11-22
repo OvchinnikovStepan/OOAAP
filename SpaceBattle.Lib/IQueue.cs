ecad17d95537c1653dc94506c8bfb5b9accd0431
Ekaterina <katya.05.2004.km@gmail.com>
2023-11-22 21:36:02 +0600

    реализация стратегии поворотов

--- SpaceBattle.Lib/IQueue.cs
+++
@@ -1,6 +0,0 @@
-namespace SpaceBattle.Lib;
-
-public interface IQueue {
-    void Add(ICommand cmd);
-    ICommand Take();
-}
---
+++ SpaceBattle.Lib/Rotate_Vector.cs
@@ -0,0 +1,27 @@
+namespace SpaceBattle.Lib;
+
+public class Rotate_Vector
+{
+    private int[] cordinates;
+    private int number_of_positions;
+    public Rotate_Vector(int number_of_positions, params int[] cordinates)
+    {
+        this.number_of_positions = number_of_positions;
+        this.cordinates = cordinates.Select(p => p % number_of_positions).ToArray();
+    }
+
+    public static Rotate_Vector operator +(Rotate_Vector vector1, Rotate_Vector vector2)
+    {
+        vector1.cordinates = vector1.cordinates.Select((p, ind) => (p + vector2.cordinates[ind])).ToArray();
+        Rotate_Vector vectorsum = new Rotate_Vector(vector1.number_of_positions, vector1.cordinates);
+        return vectorsum;
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj != null && number_of_positions == ((Rotate_Vector)obj).number_of_positions && cordinates.SequenceEqual(((Rotate_Vector)obj).cordinates);
+    }
+    public override int GetHashCode()
+    {
+        return cordinates.GetHashCode();
+    }
+}
