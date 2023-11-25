ae74e007d5513b5679046bb85b5a1726e9568555
Ekaterina <katya.05.2004.km@gmail.com>
2023-11-26 01:01:15 +0600

    реализация стратегии поворота

--- SpaceBattle.Lib.Tests/RotateCommandTest.cs
+++ SpaceBattle.Lib.Tests/RotateCommandTest.cs
@@ -4,22 +4,58 @@ using Moq;
 public class MoveCommandTest
 {
     [Fact]
-    public void MoveCommandPositive()
+    public void CommandPositive()
     {
         //pre
 
         var rotatable = new Mock<IRotatable>();
         rotatable.SetupGet(m => m.Position).Returns(new Rotate_Vector(360, 45)).Verifiable();
-        rotatable.SetupGet(m => m.Velocity).Returns(new Rotate_Vector(360, 90)).Verifiable();
+        rotatable.SetupGet(m => m.Velocity).Returns(new Rotate_Vector(360, 45)).Verifiable();
         var mc = new RotateCommand(rotatable.Object);
 
         //act
         mc.Execute();
 
-        //post
-        //movable // pos == (7, 8)
-
-        rotatable.VerifySet(m => m.Position = new Rotate_Vector(360, 135), Times.Once);
+        rotatable.VerifySet(m => m.Position = new Rotate_Vector(360, 90), Times.Once);
         rotatable.VerifyAll();
     }
+    [Fact]
+    public void Attemt_to_move_without_position_failed()
+    {
+        var movable = new Mock<IRotatable>();
+
+        movable.SetupGet(m => m.Position).Throws(new Exception()).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Rotate_Vector(360, 45)).Verifiable();
+
+        ICommand moveCommand = new RotateCommand(movable.Object);
+
+        Assert.Throws<Exception>(moveCommand.Execute);
+    }
+    [Fact]
+    public void Attemt_to_move_without_velocity_failed()
+    {
+        var movable = new Mock<IRotatable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Rotate_Vector(360, 45)).Verifiable();
+        movable.SetupGet(m => m.Velocity).Throws(new Exception()).Verifiable();
+
+        ICommand moveCommand = new RotateCommand(movable.Object);
+
+        Assert.Throws<Exception>(moveCommand.Execute);
+    }
+    [Fact]
+    public void Attemt_to_move_immoveble_object_failed()
+    {
+        var movable = new Mock<IRotatable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Rotate_Vector(360, 45)).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Rotate_Vector(360, 45)).Verifiable();
+
+        movable.SetupSet(m => m.Position).Throws(() => new Exception()).Verifiable();
+
+        ICommand moveCommand = new RotateCommand(movable.Object);
+
+        Assert.Throws<Exception>(moveCommand.Execute);
+    }
+
 }
