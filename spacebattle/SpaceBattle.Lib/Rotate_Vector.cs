f57cea991e5f7f7f48fb9033112f423a3806ee64
Ekaterina <katya.05.2004.km@gmail.com>
2023-12-01 10:54:38 +0600

    изменение названий и удаление лишних файлов

--- spacebattle/SpaceBattle.Lib/Rotate_Vector.cs
+++ spacebattle/SpaceBattle.Lib/Rotate_Vector.cs
@@ -2,26 +2,26 @@ namespace SpaceBattle.Lib;
 
 public class Rotate_Vector
 {
-    private int[] cordinates;
-    private int number_of_positions;
-    public Rotate_Vector(int number_of_positions, params int[] cordinates)
+    private int[] Cordinates;
+    private int Number_Of_Positions;
+    public Rotate_Vector(int Number_Of_Positions, params int[] Cordinates)
     {
-        this.number_of_positions = number_of_positions;
-        this.cordinates = cordinates.Select(p => p % number_of_positions).ToArray();
+        this.Number_Of_Positions = Number_Of_Positions;
+        this.Cordinates = Cordinates.Select(p => p % Number_Of_Positions).ToArray();
     }
 
     public static Rotate_Vector operator +(Rotate_Vector vector1, Rotate_Vector vector2)
     {
-        vector1.cordinates = vector1.cordinates.Select((p, ind) => (p + vector2.cordinates[ind])).ToArray();
-        Rotate_Vector vectorsum = new Rotate_Vector(vector1.number_of_positions, vector1.cordinates);
+        vector1.Cordinates = vector1.Cordinates.Select((p, ind) => (p + vector2.Cordinates[ind])).ToArray();
+        Rotate_Vector vectorsum = new Rotate_Vector(vector1.Number_Of_Positions, vector1.Cordinates);
         return vectorsum;
     }
     public override bool Equals(object obj)
     {
-        return number_of_positions == ((Rotate_Vector)obj).number_of_positions && cordinates.SequenceEqual(((Rotate_Vector)obj).cordinates);
+        return Number_Of_Positions == ((Rotate_Vector)obj).Number_Of_Positions && Cordinates.SequenceEqual(((Rotate_Vector)obj).Cordinates);
     }
     public override int GetHashCode()
     {
-        return cordinates.GetHashCode();
+        return Cordinates.GetHashCode();
     }
 }
