a017ec5bd7c1e2b67dfdec334a4be9ba16a54c1d
Ekaterina <katya.05.2004.km@gmail.com>
2023-12-13 22:15:32 +0600

    добавление интерфейсов IStopCommand, IQueue, IUObject

--- spacebattle/SpaceBattle.Lib/IStopCommand.cs
+++ spacebattle/SpaceBattle.Lib/IStopCommand.cs
@@ -3,6 +3,6 @@ namespace SpaceBattle.Lib
     public interface IStopCommand
     {
         public IUObject Target { get; }
-        public IDictionary<string> property { get; }
+        public IDictionary<string, object> property { get; }
     }
 }
