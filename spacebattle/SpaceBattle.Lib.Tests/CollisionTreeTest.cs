1058713532c864416efc3f78d7b2ed8ef9afbf88
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-12-15 01:09:18 +0600

    Регистрация "Game.Collision.Tree"

--- spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTest.cs
@@ -1,2 +1,17 @@
 using Hwdtech;
+using Hwdtech.Ioc;
 using Moq;
+using Node = System.Collections.Generic.Dictionary<int, object>;
+
+public class BuildCollisionTreeTest
+{
+    public BuildCollisionTreeTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        var collisionTree=new Node();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register","Game.Collision.Tree",(object[] args) =>{return collisionTree;}).Execute();
+    }
+}
