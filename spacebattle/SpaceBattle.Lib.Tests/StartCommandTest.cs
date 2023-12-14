56f763812bfcbaf002c41f9121fcca784bef074e
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-12-14 17:10:29 +0600

    Фиксы

--- spacebattle/SpaceBattle.Lib.Tests/StartCommandTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/StartCommandTest.cs
@@ -25,9 +25,24 @@ public class StartCommand_Tests
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register","Game.Commands.LongOperation",
         (object[] args) => {return LongMoveCommand;}).Execute();
     }
+   [Fact]
+    public void IUObject_Methods_Work_Correctly()
+    {
+      var target=new Mock<IUObject>();
+      var targetProperties = new Dictionary<string, object>();
+      target.Setup(t => t.setProperty(It.IsAny<string>(), It.IsAny<object>())).Callback<string, object>(targetProperties.Add).Verifiable();
+    
+      target.Object.setProperty("key","value");
+
+      target.Setup(t => t.getProperty(It.IsAny<string>())).Returns(targetProperties["key"]).Verifiable();
+      
+      var value=target.Object.getProperty("key");
+      
+      Assert.Equal(targetProperties["key"],value);
+    }
 
     [Fact]
-    public void StartCommand_Positive()
+    public void StartCommand_Command_Works_Correctly()
     {
         var queue = new Mock<IQueue>();
         var realQueue = new Queue<ICommand>();
@@ -56,13 +71,13 @@ public class StartCommand_Tests
     }
 
     [Fact]
-    public void StartCommand_CantSetProperties()
+    public void StartCommand_Disability_To_Read_Target_Properties_Causes_Exeption()
     {
         var startcmd = new Mock<IStartCommand>();
         var target = new Mock<IUObject>();
         var targetProperties = new Dictionary<string, object>();
 
-        var properties = new Dictionary<string, object> {{ "Position", new Vector(0,1,2,3,4,5) }};
+        var properties = new Dictionary<string, object> {{ "Position", "Vector" }};
 
         startcmd.SetupGet(s => s.Properties).Returns(properties).Verifiable();
         startcmd.SetupGet(s => s.Target).Returns(target.Object).Verifiable();
@@ -74,11 +89,11 @@ public class StartCommand_Tests
     }
 
     [Fact]
-    public void StartCommand_CantPutCommandInQueue()
+    public void StartCommand_Disability_To_Put_Command_In_Queue_Causes_Exeption()
     {
         var queue = new Mock<IQueue>();
         var realQueue = new Queue<ICommand>();
-        queue.Setup(q => q.Add(It.IsAny<ICommand>())).Callback(()=>{}).Verifiable();
+        queue.Setup(q => q.Add(It.IsAny<ICommand>())).Callback(()=> throw new Exception()).Verifiable();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register","Game.Queue",(object[] args) =>{return queue.Object;} ).Execute();
 
         var startcmd = new Mock<IStartCommand>();
@@ -91,13 +106,12 @@ public class StartCommand_Tests
         target.Setup(o => o.setProperty(It.IsAny<string>(), It.IsAny<object>())).Callback<string, object>(targetProperties.Add).Verifiable();
 
         var startCommand = new StartCommand(startcmd.Object);
-        startCommand.Execute();
 
-        Assert.Empty(realQueue);
+        Assert.Throws<Exception>(startCommand.Execute);
     }
 
     [Fact]
-    public void StartCommand_CantReadPropertiesOfOrder_FromStartable()
+    public void StartCommand_Disability_To_Read_IStartCommand_Properties_Causes_Exeption()
     {
         var startcmd = new Mock<IStartCommand>();
         startcmd.SetupGet(s => s.Properties).Throws(new Exception()).Verifiable();
@@ -107,30 +121,15 @@ public class StartCommand_Tests
     }
 
     [Fact]
-    public void StartCommand_CantReadOrder_FromStartable()
+    public void StartCommand_Disability_To_Read_Target_From_IStartCommand_Causes_Exeption()
     {
         var startcmd = new Mock<IStartCommand>();
         startcmd.SetupGet(s => s.Target).Throws(new Exception()).Verifiable();
-        var properties = new Dictionary<string, object> {{ "Position", new Vector(5,4,3,2,1,0) }};
+        var properties = new Dictionary<string, object> {{ "Position", "Vector" }};
 
         startcmd.SetupGet(s => s.Properties).Returns(properties).Verifiable();
         var startCommand = new StartCommand(startcmd.Object);
 
         Assert.Throws<Exception>(startCommand.Execute);
-    }
-    [Fact]
-    public void IUObject_Test()
-    {
-      var target=new Mock<IUObject>();
-      var targetProperties = new Dictionary<string, object>();
-      target.Setup(t => t.setProperty(It.IsAny<string>(), It.IsAny<object>())).Callback<string, object>(targetProperties.Add).Verifiable();
-    
-      target.Object.setProperty("key","value");
-
-      target.Setup(t => t.getProperty(It.IsAny<string>())).Returns(targetProperties["key"]).Verifiable();
-      
-      var value=target.Object.getProperty("key");
-      
-      Assert.Equal(targetProperties["key"],value);
     }
 }
