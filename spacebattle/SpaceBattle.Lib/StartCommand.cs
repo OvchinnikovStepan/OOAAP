fbeabec7913033325c0fc59b6602a8b0b3e28bbc
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-12-14 12:24:43 +0600

    Работа над тестами

--- spacebattle/SpaceBattle.Lib/StartCommand.cs
+++ spacebattle/SpaceBattle.Lib/StartCommand.cs
@@ -1,6 +1,41 @@
+using Hwdtech;
+
 namespace SpaceBattle.Lib;
 
 public interface IStartCommand{
     IUObject Target {get;}
     IDictionary <string,object> Properties {get;}
+}
+
+public class StartCommand : ICommand
+{
+    private readonly IStartCommand _startable;
+    public StartCommand(IStartCommand startable)
+    {
+        _startable = startable;
+    }
+
+    public void Execute()
+    {
+        _startable.Properties.ToList().ForEach(property => IoC.Resolve<object>(
+            "Game.IUObject.SetProperty",
+            _startable.Target,
+            property.Key,
+            property.Value
+        ));
+
+        var cmd = IoC.Resolve<ICommand>(
+            "Game.Commands.LongMove",
+            _startable.Target
+        );
+
+        IoC.Resolve<object>(
+            "Game.IUObject.SetProperty",
+            _startable.Target,
+            "Game.Commands.LongMove",
+            cmd
+        );
+
+        IoC.Resolve<IQueue>("Game.Queue").Add(cmd);
+    }
 }
