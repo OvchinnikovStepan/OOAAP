8d3a412c19088d80116b1aaa57b97693633f2b83
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-12-15 22:54:20 +0600

    Реализация Обработчика

---
+++ spacebattle/SpaceBattle.Lib/ExeptionHandler.cs
@@ -0,0 +1,34 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+using N=System.Collections.Generic. Dictionary<int,Dictionary<int,Handler>>;
+public interface Handler
+    {
+        object Run(params object[] args);
+    }
+public class ExceptionHandler : Handler
+    {
+        public object Run(params object[] args)
+        {
+            var command = (ICommand)args[0];
+            var commandHash=command.GetType().GetHashCode();
+            var exception = (Exception)args[1];
+            var exceptionHash=exception.GetType().GetHashCode();
+            var tree = IoC.Resolve<N>("Exeption.tree");
+
+            Dictionary<int, Handler>? subTree;
+
+            if (!tree.TryGetValue(commandHash, out subTree))
+                {
+                    subTree = IoC.Resolve<Dictionary<int, Handler>>("Exception.Get.NoCommandSubTree");
+                }
+
+            Handler? exceptionHandler;
+
+            if (!subTree.TryGetValue(exceptionHash, out exceptionHandler))
+                {
+                   return IoC.Resolve<Handler>("Exception.Get.NoExcepetionHandler");
+                }
+                
+            return exceptionHandler;
+        }
+    }
