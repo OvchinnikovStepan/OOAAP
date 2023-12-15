10b13292ffa187cfe8ac3d2da467821cddf8b390
Ovchinnikov_Stepan <ovstal@gmail.com>
2023-12-15 11:16:22 +0600

    Доделал тест

--- spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTest.cs
@@ -19,15 +19,15 @@ public class BuildCollisionTreeTest
     public void BuildCollisionTreeCommand_Command_Works_Correctly()
     {
         var reader =new Mock<CollisionArraysFromFileReader>();
-        var filepath ="../CollisionTestFile.txt";
-        var arrays = File.ReadAllLines(filepath).Select(line=>line.Split(" ").Select(int.Parse).ToList()).ToList();
+        var filepath ="../../../CollisionTestFile.txt";
+        var arrays = File.ReadAllLines(filepath).Select(line=>line.Split(" ").Select(int.Parse).ToArray()).ToList();
         reader.Setup(r=>r.ReadFile()).Returns(arrays);
 
-        var level1Expected=new HashSet<int>(){1,3,5};
-        var level2Expected=new HashSet<int>(){1,7,3,5};
-        var level3Expected=new HashSet<int>(){1,3,6};
-        var level4Expected=new HashSet<int>(){1,2,4,6};
-        var level5Expected=new HashSet<int>(){1,2,4};
+        var level1Expected=new HashSet<int>(){1,3};
+        var level2Expected=new HashSet<int>(){2,3};
+        var level3Expected=new HashSet<int>(){3};
+        var level4Expected=new HashSet<int>(){4,5,3};
+        var level5Expected=new HashSet<int>(){4,3};
 
         var collisionTreeCommand=new BuildCollisionTreeCommand(reader.Object);
 
@@ -35,17 +35,18 @@ public class BuildCollisionTreeTest
 
         var collisionTree =IoC.Resolve<Node>("Game.Collision.Tree");
 
-        var level1Derived=collisionTree.Keys;
-        var level2Derived=((Node)collisionTree[1]).Keys.Union(((Node)collisionTree[3]).Keys).Union(((Node)collisionTree[5]).Keys);;
-        var level3Derived=new HashSet<int>(){};
-        var level4Derived=new HashSet<int>(){};
-        var level5Derived=new HashSet<int>(){};
+        var level1Derived = collisionTree.Keys;
+        var level2Derived = ((Node)collisionTree[1]).Keys.Union(((Node)collisionTree[3]).Keys);
+        var level3Derived = ((Node)((Node)collisionTree[1])[2]).Keys.Union(((Node)((Node)collisionTree[3])[3]).Keys);
+        var level4Derived = ((Node)((Node)((Node)collisionTree[1])[2])[3]).Keys.Union(((Node)((Node)((Node)collisionTree[3])[3])[3]).Keys);
+        var level5Derived=((Node)((Node)((Node)((Node)collisionTree[1])[2])[3])[4]).Keys.Union(((Node)((Node)((Node)((Node)collisionTree[1])[2])[3])[5]).Keys)
+                .Union(((Node)((Node)((Node)((Node)collisionTree[3])[3])[3])[3]).Keys);
 
         Assert.True(level1Derived.SequenceEqual(level1Expected));
         Assert.True(level2Derived.SequenceEqual(level2Expected));
         Assert.True(level3Derived.SequenceEqual(level3Expected));
         Assert.True(level4Derived.SequenceEqual(level4Expected));
         Assert.True(level5Derived.SequenceEqual(level5Expected));
-
+        Assert.Empty(((Node)((Node)((Node)((Node)collisionTree[1])[2])[3])[5]).Keys);
     }
 }
