78cdd39bfc15d823a71693db4b8fbe191becef03
Ekaterina <katya.05.2004.km@gmail.com>
2023-12-16 17:50:45 +0600

    добавление тестов

--- spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTests.cs
@@ -1,62 +1,94 @@
-namespace SpaceBattle.Lib.Tests;
-using Dict = Dictionary<int, object>;
 using Hwdtech;
 using Hwdtech.Ioc;
+using Moq;
 
-public class DecisionTreeTest
+namespace SpaceBattle.Lib.Tests
 {
-    readonly string path;
-
-    public DecisionTreeTest()
+    public class CheckCollisionTests
     {
-        new InitScopeBasedIoCImplementationCommand().Execute();
+        public CheckCollisionTests()
+        {
+            new InitScopeBasedIoCImplementationCommand().Execute();
+            IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
-        IoC.Resolve<ICommand>(
-            "Scopes.Current.Set",
-            IoC.Resolve<object>("Scopes.New",
-            IoC.Resolve<object>("Scopes.Root")))
-        .Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.GetProperty", (object[] args) => new List<int> { 1, 1, 1, 1, 1 }).Execute();
 
-        var tree = new Dict();
-        IoC.Resolve<ICommand>(
-            "IoC.Register", "Game.DecisionTree",
-            (object[] args) => tree
-        ).Execute();
+            new InitFindVariationsCommand().Execute();
 
-        path = @"../../../Vector.txt";
-    }
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.CheckCollision", (object[] args) => new CollisionCheckCommand((IUObject)args[0], (IUObject)args[1])).Execute();
+        }
 
-    [Fact]
-    public void BuildDecisionTreeWithoutExceptions()
-    {
-        var decisionTreeCommand = new DecisionTreeCommand(path);
-        decisionTreeCommand.Execute();
+        [Fact]
+        public void CheckCollision_Command_Should_Not_Execute_Collision_Event_If_No_Collision_Occurs()
+        {
+            var mockCommand = new Mock<ICommand>();
+            mockCommand.Setup(c => c.Execute()).Verifiable();
 
-        var tree = IoC.Resolve<Dict>("Game.DecisionTree");
+            var mockDictionary = new Mock<IDictionary<int, object>>();
+            mockDictionary.SetupGet(d => d[It.IsAny<int>()]).Throws(new System.Collections.Generic.KeyNotFoundException()).Verifiable();
 
-        Assert.True(tree.ContainsKey(3));
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.BuildCollisionTree", (object[] args) => mockDictionary.Object).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Event.Collision", (object[] args) => mockCommand.Object).Execute();
 
-        Assert.True(((Dict)tree[3]).ContainsKey(4));
+            var mockUObject = new Mock<IUObject>();
 
-        Assert.True(((Dict)((Dict)tree[3])[4]).ContainsKey(6));
+            var checkCollisionCommand = IoC.Resolve<ICommand>("Game.Command.CheckCollision", mockUObject.Object, mockUObject.Object);
 
-        Assert.True(
-            ((Dict)((Dict)((Dict)tree[3])[4])[6]).ContainsKey(2)
-        );
-    }
+            Assert.Throws<System.Collections.Generic.KeyNotFoundException>(() => checkCollisionCommand.Execute());
+            mockDictionary.Verify(d => d[It.IsAny<int>()], Times.Once());
+            mockCommand.Verify(command => command.Execute(), Times.Never());
+        }
 
-    [Fact]
-    public void DecisionTreeCommandExecuteTest()
-    {
-        var decisionTreeCommand = new DecisionTreeCommand(path);
-        Assert.NotNull(decisionTreeCommand);
-    }
 
-    [Fact]
-    public void ImpossibleToReadFileWhenBuildDecisionTree()
-    {
-        var wrongPath = "wrongVectors";
-        var decisionTreeCommand = new DecisionTreeCommand(wrongPath);
-        Assert.Throws<FileNotFoundException>(decisionTreeCommand.Execute);
+        [Fact]
+        public void CheckCollision_Command_Should_Execute_Collision_Event_If_Collision_Occurs()
+        {
+
+            var mockCommand = new Mock<ICommand>();
+            mockCommand.Setup(c => c.Execute()).Verifiable();
+
+            var mockDictionary = new Mock<IDictionary<int, object>>();
+            mockDictionary.SetupGet(d => d[It.IsAny<int>()]).Returns(mockDictionary.Object);
+
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.BuildCollisionTree", (object[] args) => mockDictionary.Object).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Event.Collision", (object[] args) => mockCommand.Object).Execute();
+
+            var mockUObject = new Mock<IUObject>();
+
+            var checkCollisionCommand = IoC.Resolve<ICommand>("Game.Command.CheckCollision", mockUObject.Object, mockUObject.Object);
+
+            Assert.NotNull(checkCollisionCommand);
+            Assert.IsType<CollisionCheckCommand>(checkCollisionCommand);
+            checkCollisionCommand.Execute();
+        }
+
+        [Fact]
+        public void CheckCollision_Command_Should_Return_Valid_Collision_Tree_If_Collision_Occurs()
+        {
+            var mockCommand = new Mock<ICommand>();
+            mockCommand.Setup(c => c.Execute()).Verifiable();
+
+            var mockDictionary = new Mock<IDictionary<int, object>>();
+            mockDictionary.SetupGet(d => d[It.IsAny<int>()]).Returns(new Dictionary<int, object>()).Verifiable();
+
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.BuildCollisionTree", (object[] args) => mockDictionary.Object).Execute();
+            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Event.Collision", (object[] args) => mockCommand.Object).Execute();
+
+            var mockUObject = new Mock<IUObject>();
+
+            var checkCollisionCommand = IoC.Resolve<ICommand>("Game.Command.CheckCollision", mockUObject.Object, mockUObject.Object);
+
+            Assert.Throws<System.Collections.Generic.KeyNotFoundException>(() => { checkCollisionCommand.Execute(); });
+
+            mockDictionary.Verify(d => d[It.IsAny<int>()], Times.Once());
+            mockCommand.Verify(command => command.Execute(), Times.Never());
+        }
+
+        [Fact]
+        public void InitFindVariationsCommand_Should_Initialize_FindVariationsCommand()
+        {
+            var findVariationsCommand = new InitFindVariationsCommand();
+            Assert.NotNull(findVariationsCommand);
+        }
     }
 }
