35ddb9ec075367c53980024a9f1c0bdfac4bf61c
Ekaterina <katya.05.2004.km@gmail.com>
2023-12-28 23:18:44 +0600

    исправление теста

--- spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/CollisionTreeTests.cs
@@ -59,28 +59,7 @@ namespace SpaceBattle.Lib.Tests
             Assert.NotNull(checkCollisionCommand);
             Assert.IsType<CollisionCheckCommand>(checkCollisionCommand);
             checkCollisionCommand.Execute();
-        }
-
-        [Fact]
-        public void CheckCollision_Command_Should_Return_Valid_Collision_Tree_If_Collision_Occurs()
-        {
-            var mockCommand = new Mock<ICommand>();
-            mockCommand.Setup(c => c.Execute()).Verifiable();
-
-            var mockDictionary = new Mock<IDictionary<int, object>>();
-            mockDictionary.SetupGet(d => d[It.IsAny<int>()]).Returns(new Dictionary<int, object>()).Verifiable();
-
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Command.BuildCollisionTree", (object[] args) => mockDictionary.Object).Execute();
-            IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Event.Collision", (object[] args) => mockCommand.Object).Execute();
-
-            var mockUObject = new Mock<IUObject>();
-
-            var checkCollisionCommand = IoC.Resolve<ICommand>("Game.Command.CheckCollision", mockUObject.Object, mockUObject.Object);
-
-            Assert.Throws<System.Collections.Generic.KeyNotFoundException>(() => { checkCollisionCommand.Execute(); });
-
-            mockDictionary.Verify(d => d[It.IsAny<int>()], Times.Once());
-            mockCommand.Verify(command => command.Execute(), Times.Never());
+            mockCommand.Verify(m => m.Execute(), Times.Once());
         }
 
         [Fact]
