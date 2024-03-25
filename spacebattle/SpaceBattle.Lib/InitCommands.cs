7e7036e7451186e98c60e65ffdfa1fddbbbefd29
Ekaterina <katya.05.2004.km@gmail.com>
2024-03-26 05:04:30 +0600

    устранение конфликтов

---
+++ spacebattle/SpaceBattle.Lib/InitCommands.cs
@@ -0,0 +1,49 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+public class InitCommand : Hwdtech.ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.StartServerCommand", (object[] args) =>
+        {
+            var NumberOfThreads = (int)args[0];
+
+            return new ActionCommand(() =>
+            {
+                Enumerable.Range(0, NumberOfThreads).ToList().ForEach(i =>
+                {
+                    IoC.Resolve<Hwdtech.ICommand>("Game.Commands.CreateAndStartThread").Execute();
+                });
+            });
+        }).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.ExeptionHandler", (object[] args) =>
+       {
+           var errorFile = IoC.Resolve<string>("GetLogFilePath");
+           return new ActionCommand(() =>
+           {
+               using (var sw = File.AppendText(errorFile))
+               {
+                   sw.WriteLine($"Error occurred in command: {args[0]}");
+                   sw.WriteLine($"Exception: {args[1]}");
+               }
+           });
+       }).Execute();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.StopServerCommand", (object[] args) =>
+        {
+            var ThreadList = IoC.Resolve<List<int>>("Game.Commands.GetThreadIDs");
+            return new ActionCommand(() =>
+            {
+                foreach (var thread_id in ThreadList)
+                {
+                    IoC.Resolve<Hwdtech.ICommand>("Game.Commands.SendCommand", thread_id,
+                        IoC.Resolve<Hwdtech.ICommand>("Game.Commands.SoftStopThread", thread_id,
+                            IoC.Resolve<Action>("Game.Commands.StopServerBarrierRemove"))).Execute();
+                }
+
+                IoC.Resolve<Hwdtech.ICommand>("Game.Commands.StopServerBarrierWait").Execute();
+            });
+        }).Execute();
+    }
+}
