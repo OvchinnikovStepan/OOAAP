7532bc03ed90f4d4c9bc1aa8485dfc865dd5feea
Ovchinnikov_Stepan <ovstal@gmail.com>
2024-04-24 13:34:35 +0600

    trying

--- spacebattle/CoreWCF/Startup.cs
+++ spacebattle/CoreWCF/Startup.cs
@@ -1,50 +1,50 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using CoreWCF;
-using CoreWCF.Configuration;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using SpaceBattle.WebHttp;
-using Swashbuckle.AspNetCore.Swagger;
+﻿// using System.Diagnostics.CodeAnalysis;
+// using CoreWCF;
+// using CoreWCF.Configuration;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.Extensions.DependencyInjection;
+// using SpaceBattle.WebHttp;
+// using Swashbuckle.AspNetCore.Swagger;
 
-[ExcludeFromCodeCoverage]
-internal sealed class Startup
-{
-    [ExcludeFromCodeCoverage]
-    public static void ConfigureServices(IServiceCollection services)
-    {
-        services.AddServiceModelWebServices(o =>
-        {
-            o.Title = "Space Battle API";
-            o.Version = "1";
-            o.Description = "API Description";
-            o.TermsOfService = new("http://spacebattle.com/terms");
-            o.ContactName = "Contact";
-            o.ContactEmail = "support@spacebattle.com";
-            o.ContactUrl = new("http://spacebattle.com/contact");
-            o.ExternalDocumentUrl = new("http://spacebattle.com/doc.pdf");
-            o.ExternalDocumentDescription = "Documentation";
-        });
+// [ExcludeFromCodeCoverage]
+// internal sealed class Startup
+// {
+//     [ExcludeFromCodeCoverage]
+//     public static void ConfigureServices(IServiceCollection services)
+//     {
+//         services.AddServiceModelWebServices(o =>
+//         {
+//             o.Title = "Space Battle API";
+//             o.Version = "1";
+//             o.Description = "API Description";
+//             o.TermsOfService = new("http://spacebattle.com/terms");
+//             o.ContactName = "Contact";
+//             o.ContactEmail = "support@spacebattle.com";
+//             o.ContactUrl = new("http://spacebattle.com/contact");
+//             o.ExternalDocumentUrl = new("http://spacebattle.com/doc.pdf");
+//             o.ExternalDocumentDescription = "Documentation";
+//         });
 
-        services.AddSingleton(new SwaggerOptions());
-    }
-    [ExcludeFromCodeCoverage]
-    public static void Configure(IApplicationBuilder app)
-    {
-        app.UseMiddleware<SwaggerMiddleware>();
-        app.UseSwaggerUI();
+//         services.AddSingleton(new SwaggerOptions());
+//     }
+//     [ExcludeFromCodeCoverage]
+//     public static void Configure(IApplicationBuilder app)
+//     {
+//         app.UseMiddleware<SwaggerMiddleware>();
+//         app.UseSwaggerUI();
 
-        app.UseServiceModel(builder =>
-        {
-            builder.AddService<WebApi>();
-            builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
-            {
-                MaxReceivedMessageSize = 5242880,
-                MaxBufferSize = 65536,
-            }, "api", behavior =>
-            {
-                behavior.HelpEnabled = true;
-                behavior.AutomaticFormatSelectionEnabled = true;
-            });
-        });
-    }
-}
+//         app.UseServiceModel(builder =>
+//         {
+//             builder.AddService<WebApi>();
+//             builder.AddServiceWebEndpoint<WebApi, IWebApi>(new WebHttpBinding
+//             {
+//                 MaxReceivedMessageSize = 5242880,
+//                 MaxBufferSize = 65536,
+//             }, "api", behavior =>
+//             {
+//                 behavior.HelpEnabled = true;
+//                 behavior.AutomaticFormatSelectionEnabled = true;
+//             });
+//         });
+//     }
+// }
