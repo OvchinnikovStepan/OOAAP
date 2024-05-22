dd17c66ccdab8ed164d41f8b568f8e1c206ae4d4
Ovchinnikov_Stepan <ovstal@gmail.com>
2024-05-22 17:39:51 +0600

    Start

---
+++ spacebattle/SpaceBattle.Lib/GameCommand.cs
@@ -0,0 +1,37 @@
+﻿namespace SpaceBattle.Lib;
+using System.Diagnostics;
+using Hwdtech;
+public class GameCommand : ICommand
+{
+    private readonly CmdSource Source;
+    private readonly object Scope;
+    private readonly Stopwatch stopwatch;
+    public GameCommand(CmdSource Source, object Scope)
+    {
+        this.Source = Source;
+        this.Scope = Scope;
+        stopwatch = new Stopwatch();
+    }
+
+    public void Execute()
+    {
+        var quant = (double)IoC.Resolve<object>("GetGameQuant");
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Scope).Execute();
+        stopwatch.Start();
+
+        while ((quant >= stopwatch.ElapsedMilliseconds) && (!Source.IsEmpty()))
+        {
+            var command = Source.Take();
+            try
+            {
+                command.Execute();
+            }
+            catch (Exception e)
+            {
+                IoC.Resolve<Hwdtech.ICommand>("Game.Commands.ExceptionHandler", command, e).Execute();
+            }
+        }
+
+        stopwatch.Reset();
+    }
+}
