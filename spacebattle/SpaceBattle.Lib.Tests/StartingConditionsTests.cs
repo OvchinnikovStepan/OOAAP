469a925fef55387ee8169bb641d10bcee326f487
Ovchinnikov_Stepan <ovstal@gmail.com>
2024-05-29 00:14:46 +0600

    Test: SetFuelCommand is tested & new test for ArrangeOneSpaceShipCommand & small test fixes

--- spacebattle/SpaceBattle.Lib.Tests/StartingConditionsTests.cs
+++ spacebattle/SpaceBattle.Lib.Tests/StartingConditionsTests.cs
@@ -74,7 +74,7 @@ public class StartConditionsTests
         iterator.SetupGet(x => x.Current).Returns(new Vector(1, 1)).Verifiable();
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
         {
-            return new ActionCommand(() => { somecommand.Object.Execute(); });
+            return somecommand.Object;
         }).Execute();
         new ArrangeOneSpaceShipCommand(obj.Object, iterator.Object).Execute();
         somecommand.Verify(somecommand => somecommand.Execute(), Times.Once());
@@ -93,7 +93,27 @@ public class StartConditionsTests
         iterator.SetupGet(x => x.Current).Throws(new Exception());
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
         {
-            return new ActionCommand(() => { somecommand.Object.Execute(); });
+            return somecommand.Object;
+        }).Execute();
+        Assert.Throws<Exception>(() => { new ArrangeOneSpaceShipCommand(obj.Object, iterator.Object).Execute(); });
+    }
+    [Fact]
+    public void Test_Arranging_One_SpaceShip_Disabbility_To_Create_Set_Property_Command_Cause_Exception()
+    {
+        var obj = new Mock<IUObject>();
+        var somecommand = new Mock<Hwdtech.ICommand>();
+        var iterator = new Mock<IEnumerator<object>>();
+
+        var exeptionCmd = new Mock<Hwdtech.ICommand>();
+        exeptionCmd.Setup(x => x.Execute()).Throws(new Exception());
+
+        somecommand.Setup(x => x.Execute()).Verifiable();
+        iterator.Setup(x => x.MoveNext()).Verifiable();
+        iterator.SetupGet(x => x.Current).Returns(new Vector(1, 1));
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
+        {
+            exeptionCmd.Object.Execute();
+            return somecommand.Object;
         }).Execute();
         Assert.Throws<Exception>(() => { new ArrangeOneSpaceShipCommand(obj.Object, iterator.Object).Execute(); });
     }
@@ -112,8 +132,7 @@ public class StartConditionsTests
         iterator.SetupGet(x => x.Current).Returns(new Vector(1, 1));
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
         {
-            exeptionCmd.Object.Execute();
-            return new ActionCommand(() => { somecommand.Object.Execute(); });
+            return exeptionCmd.Object;
         }).Execute();
         Assert.Throws<Exception>(() => { new ArrangeOneSpaceShipCommand(obj.Object, iterator.Object).Execute(); });
     }
@@ -129,7 +148,7 @@ public class StartConditionsTests
         iterator.SetupGet(x => x.Current).Returns(new Vector(1, 1));
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
         {
-            return new ActionCommand(() => { somecommand.Object.Execute(); });
+            return somecommand.Object;
         }).Execute();
         Assert.Throws<Exception>(() => { new ArrangeOneSpaceShipCommand(obj.Object, iterator.Object).Execute(); });
     }
@@ -155,7 +174,7 @@ public class StartConditionsTests
     }
 
     [Fact]
-    public void Test_Arranging_SpaceShips_Dissability_To_Get_Iterator_Cause_Exception()
+    public void Test_Arranging_SpaceShips_Disability_To_Get_Iterator_Cause_Exception()
     {
         var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
         var iterator = new Mock<IEnumerator<object>>();
@@ -178,7 +197,7 @@ public class StartConditionsTests
         Assert.Throws<Exception>(() => { new ArrangeSpaceShipsCommand(spaceships).Execute(); });
     }
     [Fact]
-    public void Test_Arranging_SpaceShips_Dissability_To_Arrange_Spaceship_Cause_Exception()
+    public void Test_Arranging_SpaceShips_Disability_To_Arrange_Spaceship_Cause_Exception()
     {
         var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
         var iterator = new Mock<IEnumerator<object>>();
@@ -201,7 +220,7 @@ public class StartConditionsTests
         Assert.Throws<Exception>(() => { new ArrangeSpaceShipsCommand(spaceships).Execute(); });
     }
     [Fact]
-    public void Test_Arranging_SpaceShips_Dissability_To_Reset_Iterator_Cause_Exception()
+    public void Test_Arranging_SpaceShips_Disability_To_Reset_Iterator_Cause_Exception()
     {
         var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
         var iterator = new Mock<IEnumerator<object>>();
@@ -223,7 +242,7 @@ public class StartConditionsTests
         Assert.Throws<Exception>(() => { new ArrangeSpaceShipsCommand(spaceships).Execute(); });
     }
     [Fact]
-    public void Test_Arranging_SpaceShips_Dissability_To_Execute_Arrange_Command_Cause_Exception()
+    public void Test_Arranging_SpaceShips_Disability_To_Execute_Arrange_Command_Cause_Exception()
     {
         var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
         var iterator = new Mock<IEnumerator<object>>();
@@ -241,5 +260,50 @@ public class StartConditionsTests
             return exeptionCmd.Object;
         }).Execute();
         Assert.Throws<Exception>(() => { new ArrangeSpaceShipsCommand(spaceships).Execute(); });
+    }
+    [Fact]
+    public void Test_Setting_Fuel_Successfully()
+    {
+        var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
+        var somecommand = new Mock<Hwdtech.ICommand>();
+
+        somecommand.Setup(x => x.Execute()).Verifiable();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
+        {
+            return somecommand.Object;
+        }).Execute();
+        new SetFuelCommand(spaceships, 10).Execute();
+        somecommand.Verify(x => x.Execute(), Times.Exactly(2));
+    }
+    [Fact]
+    public void Test_Setting_Fuel_Disabbility_To_Set_Property_Cause_Exception()
+    {
+        var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
+
+        var exeptionCmd = new Mock<Hwdtech.ICommand>();
+        exeptionCmd.Setup(x => x.Execute()).Throws(new Exception());
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
+        {
+            return exeptionCmd.Object;
+        }).Execute();
+        Assert.Throws<Exception>(() => { new SetFuelCommand(spaceships, 10).Execute(); });
+    }
+    [Fact]
+    public void Test_Setting_Fuel_Disabbility_To_Create_Set_Property_Command_Cause_Exception()
+    {
+        var spaceships = new List<IUObject>() { new Mock<IUObject>().Object, new Mock<IUObject>().Object };
+        var somecommand = new Mock<Hwdtech.ICommand>();
+
+        somecommand.Setup(x => x.Execute()).Verifiable();
+        var exeptionCmd = new Mock<Hwdtech.ICommand>();
+        exeptionCmd.Setup(x => x.Execute()).Throws(new Exception());
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.UObject.SetProperty", (object[] args) =>
+        {
+            exeptionCmd.Object.Execute();
+            return somecommand.Object;
+        }).Execute();
+        Assert.Throws<Exception>(() => { new SetFuelCommand(spaceships, 10).Execute(); });
     }
 }
