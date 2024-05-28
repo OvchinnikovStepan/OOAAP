e65474872407b3a945fdca43efb89e8160ba08e7
Ovchinnikov_Stepan <ovstal@gmail.com>
2024-05-28 17:44:24 +0600

    Main_code_adding: PositionIterator

---
+++ spacebattle/SpaceBattle.Lib/PositionIterator.cs
@@ -0,0 +1,24 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class PositionIterator : IEnumerator<object> 
+{
+    private readonly IList<Vector> positions;
+    private int position_index=0;
+
+    public PositionIterator()
+    {
+        positions = IoC.Resolve<List<Vector>>("Game.StartShipPositions");
+    }
+
+    public object Current => positions[position_index];
+
+    public bool MoveNext(){
+        position_index=position_index+1;
+        return position_index < positions.Count;
+        }
+
+    public void Reset() {position_index = 0;}
+
+    public void Dispose() {throw new NotImplementedException();}
+}
