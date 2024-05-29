37d1c1f5f64b2faf6108640faef52c4e48001861
Ekaterina <katya.05.2004.km@gmail.com>
2024-05-29 19:50:53 +0600

    Test: creating base for tests

---
+++ spacebattle/SpaceBattle.Lib.Tests/InitializeGameTests.cs
@@ -0,0 +1,16 @@
+﻿using Hwdtech;
+using Hwdtech.Ioc;
+namespace SpaceBattle.Lib.Test;
+public class InitializeGameTests
+{
+    public InitializeGameTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+    [Fact]
+    public void Start_Server_Test()
+    {
+
+    }
+}
