3044ac5be5980ad12124c5ca949feea9a65fcede
Ekaterina <katya.05.2004.km@gmail.com>
2024-05-30 03:39:29 +0600

    feat: CreateEndMovementCommandStrategy & CreateStartMovementCommandStrategy

---
+++ spacebattle/SpaceBattle.Lib/CreateStartMovementCommandStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle.Lib;
+
+public class CreateStartMovementCommandStrategy : IStrategy
+{
+    public object Run(object[] args)
+    {
+        var obj = args[0];
+        return new StartCommand(IoC.Resolve<IStartCommand>("Game.IUObject.Adapter.IStartCommand", obj));
+    }
+}
