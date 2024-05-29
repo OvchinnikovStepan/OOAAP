c822399534e489438e1ac5ac96cdd7a84ca5f2b9
Ekaterina <katya.05.2004.km@gmail.com>
2024-05-29 19:06:19 +0600

    new Strategy: QueuePopStrategy; base of CreateGameScopeStrategy

---
+++ spacebattle/SpaceBattle.Lib/CreateGameScopeStrategy.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceBattle.Lib;
+
+using Hwdtech;
+
+public class NewGameScopeStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var gameId = (string)args[0];
+        var parentScope = (object)args[1];
+        var gameQueue = (object)args[2];
+        var quantum = (double)args[3];
+
+        var gameScope = IoC.Resolve<object>("Scopes.New", parentScope);
+
+        var gameScopeMap = IoC.Resolve<IDictionary<string, object>>("Game.Scope.Map");
+        gameScopeMap.Add(gameId, gameScope);
+
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", gameScope).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Quant", (object[] args) => (object)quantum).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue", (object[] args) => gameQueue);
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Queue.Pop", (object[] args) => { return new QueuePopStrategy().Run(args); });
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", parentScope).Execute();
+
+        return gameScope;
+    }
+}
---
+++ spacebattle/SpaceBattle.Lib/QueuePopStrategy.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class QueuePopStrategy : IStrategy
+{
+    public object Run(object[] args)
+    {
+        var queue = IoC.Resolve<IQueue>("Game.Queue");
+        return queue.Take();
+    }
+}
