948bbadb62d769d559efa7039cda4427bdd2808b
Ekaterina <katya.05.2004.km@gmail.com>
2024-05-29 18:22:16 +0600

    new strategy: CreateGameStrategy

---
+++ spacebattle/SpaceBattle.Lib/CreateGameStrategy.cs
@@ -0,0 +1,27 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class CreateGameStrategy : IStrategy
+{
+    public object Run(params object[] args)
+    {
+        var gameId = (string)args[0];
+        var parentScope = (object)args[1];
+        var quant = (double)args[2];
+
+        var gameScope = IoC.Resolve<object>("Game.Scope.New", gameId, parentScope, quant);
+        var gameQueue = IoC.Resolve<object>("Game.Queue.New");
+        var gameCommand = IoC.Resolve<Hwdtech.ICommand>("Game.Command", gameQueue, gameScope);
+
+        var commandsList = new List<Hwdtech.ICommand> { gameCommand };
+        var macroCommand = IoC.Resolve<Hwdtech.ICommand>("Game.Command.Macro", commandsList);
+        var injectCommand = IoC.Resolve<Hwdtech.ICommand>("Game.Command.Inject", macroCommand);
+        var repeatCommand = IoC.Resolve<Hwdtech.ICommand>("Command.Concurrent.Repeat", injectCommand);
+        commandsList.Add(repeatCommand);
+
+        var gamesList = IoC.Resolve<IDictionary<string, Hwdtech.ICommand>>("Game.GetGamesList");
+        gamesList.Add(gameId, injectCommand);
+
+        return injectCommand;
+    }
+}
