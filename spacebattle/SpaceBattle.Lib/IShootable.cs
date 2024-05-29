080d45c97eff43a23892ad0a9fb15a69a05ac31a
Ekaterina <katya.05.2004.km@gmail.com>
2024-05-30 01:54:39 +0600

    feat: new interface IShootable

---
+++ spacebattle/SpaceBattle.Lib/IShootable.cs
@@ -0,0 +1,7 @@
+﻿namespace SpaceBattle.Lib;
+public interface IShootable
+{
+    public string BulletType { get; set; }
+    public Vector Position { get; set; }
+    public Vector Velocity { get; }
+}
